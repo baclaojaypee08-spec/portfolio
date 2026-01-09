b117c6c279c8110733e7e1733154a036964d9de2
Jaypee Baclao <baclaojaypee08@gmail.com>
2026-01-09 16:45:05 +0800

    Update project files

--- my-portfolio/src/components/SectionHeader.jsx
+++ my-portfolio/src/components/SectionHeader.jsx
@@ -1,6 +1,10 @@
 function SectionHeader({ icon: Icon, title }) {
   return (
-    <h2 className="text-lg font-semibold flex items-center gap-2 mb-4">
+    <h2 className="
+        text-base        /* mobile */
+        sm:text-lg
+        md:text-xl 
+        font-semibold flex items-center gap-2 mb-4">
       <Icon className="w-7 h-7 p-1 rounded-full transition-transform duration-200 hover:scale-110 rounded-full bg-gray-100 dark:bg-slate-700" />
       <span>{title}</span>
     </h2>
