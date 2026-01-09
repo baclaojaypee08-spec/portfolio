90cb4fccf0fd3fae9ce3380f309fda6c78af445f
Jaypee Baclao <baclaojaypee08@gmail.com>
2026-01-09 16:47:16 +0800

    Update project files

--- my-portfolio/src/components/SectionHeader.jsx
+++ my-portfolio/src/components/SectionHeader.jsx
@@ -1,9 +1,9 @@
 function SectionHeader({ icon: Icon, title }) {
   return (
     <h2 className="
-        text-base        /* mobile */
-        sm:text-lg
-        md:text-xl 
+        text-xl       /* mobile */
+        sm:text-md
+        md:text-lg
         font-semibold flex items-center gap-2 mb-4">
       <Icon className="w-7 h-7 p-1 rounded-full transition-transform duration-200 hover:scale-110 rounded-full bg-gray-100 dark:bg-slate-700" />
       <span>{title}</span>
